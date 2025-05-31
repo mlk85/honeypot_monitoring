5e36c6fdecd68904fa835158cf335498fe11f3ab
mlk85 <malaczek85@interia.pl>
2025-05-31 21:45:30 +0200

    Saving results extended to google sheets

--- monitor.py
+++ monitor.py
@@ -1,14 +1,18 @@
 import csv
 import time
 import os
+import gspread
 
 from web3 import Web3
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 load_dotenv()
 
 CONTRACT_ADDRESS = os.getenv('CONTACT_ADDRESS')
 INFURA_URL = os.getenv('INFURA_URL')
+SHEET_ID = os.getenv('SHEET_ID')
 
 # ABI (skrót — tylko potrzebne eventy)
 ABI = [
@@ -35,35 +39,53 @@ ABI = [
     }
 ]
 
+#Web3
 web3 = Web3(Web3.HTTPProvider(INFURA_URL))
 contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=ABI)
+
+#Google sheets
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open_by_key(SHEET_ID).sheet1
+
+if sheet.row_count == 0 or sheet.cell(1, 1).value is None:
+    sheet.append_row(["type", "attacker", "value", "attemptNum", "wasBlacklisted", "timestamp"])
+
 
 print(f"Monitoring honeypot at: {CONTRACT_ADDRESS}")
 print("Waiting for events...")
 
-with open("honeypot_logs.csv", mode="a", newline="")as file:
-    w = csv.writer(file)
-    w.writerow(["type", "attacker", "value", "attemptNum", "wasBlacklisted", "timestamp"])
 
-    last_block = web3.eth.block_number
-    while True:
-        try:
-            current_block = web3.eth.block_number
-            if current_block > last_block:
-                logs = contract.events.AttemptDetailed.get_logs(from_block=last_block+1, to_block=current_block)
-                for log in logs:
-                    w.writerow(["AttemptDetailed", log.args.attacker, log.args.value, log.args.attemptNum,
-                                log.args.wasBlacklisted, log.args.timestamp])
-                    print(f"Attempt: {log.args.attacker} | {log.args.value} wei")
-                logs2 = contract.events.Caught.get_logs(from_block=last_block+1, to_block=current_block)
-                for log in logs2:
-                    w.writerow(["Caught", log.args.attacker, "", "", "", log.args.timestamp])
-                    print(f"Caught: {log.args.attacker}")
-                last_block = current_block
-            time.sleep(15)
-        except Exception as e:
-            print(f"Error: ", e)
-            web3 = Web3(Web3.HTTPProvider(INFURA_URL))
-            contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=ABI)
-            time.sleep(15)
+last_block = web3.eth.block_number
+while True:
+    try:
+        current_block = web3.eth.block_number
+        if current_block > last_block:
+            logs = contract.events.AttemptDetailed.get_logs(from_block=last_block + 1, to_block=current_block)
+            for log in logs:
+                sheet.append_row([
+                    "AttemptDetailed",
+                    log.args.attacker,
+                    log.args.value,
+                    log.args.attemptNum,
+                    str(log.args.wasBlacklisted),
+                    log.args.timestamp
+                ])
+                print(f"Attempt: {log.args.attacker} | {log.args.value} wei")
+            logs2 = contract.events.Caught.get_logs(from_block=last_block + 1, to_block=current_block)
+            for log in logs2:
+                sheet.append_row([
+                    "Caught",
+                    log.args.attacker,
+                    "", "", "", log.args.timestamp
+                ])
+                print(f"Caught: {log.args.attacker}")
+            last_block = current_block
+        time.sleep(15)
+    except Exception as e:
+        print(f"Error: ", e)
+        web3 = Web3(Web3.HTTPProvider(INFURA_URL))
+        contract = web3.eth.contract(address=CONTRACT_ADDRESS, abi=ABI)
+        time.sleep(15)
 
